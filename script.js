16a016c32d0db849d86e097b22244f69d6419fe1
SNaou <S.Naou@icloud.com>
2023-01-09 17:08:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+if (age >= 18 && age <= 25) {
+    console.log("You get 50% off!");
+  } else {
+    console.log("Sorry, you do not qualify for the discount.");
+  }
+  
+const firstName = "Sarah";
+
+if (firstName === "Sarah" || firstName === "Bram") {
+  console.log("Congratulations, you get a free beer!");
+} else {
+  console.log("Sorry, you do not qualify for a free beer.");
+}
+
+const totalAmount = 75;
+
+if (totalAmount > 100) {
+  console.log("Congratulations! You get a free bottle of champagne!");
+} else if (totalAmount > 50) {
+  console.log("Congratulations! You get a free portion of nachos!");
+} else if (totalAmount > 25) {
+  console.log("Congratulations! You get free bitterballen!");
+} else {
+  console.log("Sorry, you do not qualify for any free gifts.");
+}
